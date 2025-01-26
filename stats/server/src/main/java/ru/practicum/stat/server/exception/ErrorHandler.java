02b17be8f09c18bc84b81fd44dc1606a5ff4421d
Altana Batueva <altana.batueva0@gmail.com>
2025-01-26 20:22:07 +0100

    remove import

--- stats/server/src/main/java/ru/practicum/stat/server/exception/ErrorHandler.java
+++ stats/server/src/main/java/ru/practicum/stat/server/exception/ErrorHandler.java
@@ -1,7 +1,6 @@
 package ru.practicum.stat.server.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
